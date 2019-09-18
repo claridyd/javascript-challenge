824373df3355a71c3db2373df61c10d4f52c39bd
Nisey C <denise.claridy@gmail.com>
2019-09-18 19:53:10 -0400

    append table and add new rows of data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
-// from data.js
-var tableData = data;
+// Assign the data from data.js to a variable
+var ufo_data = data;
 
 // YOUR CODE HERE!
+//Reference for table body
+var tbody = d3.select('tbody');
+
+//Append on table row 'tr' for each item in array
+ufo_data.forEach((addData) => {
+    var row = tbody.append('tr');
+    //append cell to the row for each value
+    Object.entries(addData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    })
+});
+
