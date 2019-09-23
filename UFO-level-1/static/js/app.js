523f23744ef57af6eab33fc09465507ab99a6abc
Nisey C <denise.claridy@gmail.com>
2019-09-23 12:05:32 -0400

    data filter working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,3 +15,31 @@ ufo_data.forEach((addData) => {
     })
 });
 
+//select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    d3.event.preventDefault();
+    
+    var inputElement = d3.select("#datetime");
+    //Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    var filteredData = ufo_data.filter(ufo_data => ufo_data.datetime === inputValue);
+    console.log(filteredData);
+    tbody.html("");
+    filteredData.forEach((data) => {
+        
+        var row = tbody.append('tr');
+        Object.entries(data).forEach(([key,value]) => {
+            var cell = row.append('td');
+            cell.text(value);
+        })
+    });
+    
+  
+    
+});
+
+
+
